a18f9d1e44328a9795d23fc255a5d24cd96edf5a
Julien Antony <coupdebol@gmail.com>
2015-08-31 14:22:59 +0000

    script to init tables in DB

---
+++ database.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Debol on 31-Aug-15.
+ */
+
+var pg = require('pg'); //PostgreSQL
+
+var connectionString = process.env.DATABASE_URL;
+console.log("ConnectionString is: ");
+
+
+
+pg.connect(connectionString, function(err, client) {
+    if (err) throw err;
+
+    client
+        .query('CREATE TABLE name-dob(ID int NOT NULL AUTO_INCREMENT, name VARCHAR(40) not null, dob DATE)')
+        .on('end', function(row) {
+            console.log(JSON.stringify(row));
+        })
+    ;
+});
+
+
+
+
+
+//var client = new pg.Client(connectionString);
+//client.connect();
+//var query = client.query('CREATE TABLE name-dob(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+//query.on('end', function() { client.end(); });
