adbb76b9fb4b60fd1c0ea5cad09b2f2d8b4278b3
Julien Antony <coupdebol@gmail.com>
2015-08-31 09:14:49 +0000

    listen to port 80

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 80;        // set our port
 
 var jade = require('jade');
 
