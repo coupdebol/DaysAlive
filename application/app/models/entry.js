88fcc67e51e5cabf0e9995b24b32201d8da28c46
Julien Antony <coupdebol@gmail.com>
2015-09-05 22:43:40 +1000

    added extra field on DB and form

--- application/app/models/entry.js
+++ application/app/models/entry.js
@@ -4,9 +4,11 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var EntrySchema   = new Schema({
-    name: String ,
+    given_name: String ,
+    family_name: String,
     date_of_birth: String,
-    timestamp: String
+    timestamp: String,
+
 });
 
 module.exports = mongoose.model('Entry', EntrySchema);
